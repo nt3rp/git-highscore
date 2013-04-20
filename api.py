7dd1c67a384ca97a774093231783d04c414c243f
Nicholas Terwoord <nicholas.terwoord+code@gmail.com>
2013-04-20 15:38:16 -0400

    Started creating resources
    - Apparently, python git libraries suck?
        - At least, the docs do

--- api.py
+++ api.py
@@ -4,11 +4,24 @@ from flask.ext import restful
 app = Flask(__name__)
 api = restful.Api(app)
 
-class HelloWorld(restful.Resource):
+# TODO (Resources):
+# - Title (held by some for some measurable feat, one user)
+# - Rank (similar to title, but for less prestigious things, multiple users)
+# - Achievements (not restricted to one user)
+# - Users
+
+class Commit(restful.Resource):
+    def get(self, id):
+
+        return {'hello': 'world'}
+
+
+class CommitList(restful.Resource):
     def get(self):
         return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(CommitList, '/commits')
+api.add_resource(Commit, '/commits/<string:id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
