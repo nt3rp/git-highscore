c3619590a31e9b35db6401453844918d509bd52b
Nicholas Terwoord <nicholas.terwoord+code@gmail.com>
2013-04-21 00:35:56 -0400

    Added means to access repository
    - Actually can retrieve commits!

--- git_highscore/resources.py
+++ git_highscore/resources.py
@@ -1,3 +1,5 @@
+import git
+from gitdb.exc import BadObject
 from flask.ext import restful
 
 # TODO (Resources):
@@ -6,9 +8,29 @@ from flask.ext import restful
 # - Achievements (not restricted to one user)
 # - Users
 
+# For now, just use a really dumb model
+# Can obtain active branch via model.active_branch
+# getattr(model.heads, 'branch_name') to try to get other branches
+
+model = git.Repo('.')
+
+# TODO: What format do we want results to be?
+# TODO: How to do pagination with flask-restful?
+
 class Commit(restful.Resource):
     def get(self, id):
-        return {'commit': 'blah'}
+        try:
+            commit = model.commit(id)
+        except BadObject:
+            # TODO: Error
+            pass
+        else:
+            return {
+                'commit': {
+                    'author': commit.author.email,
+                    'message': commit.message
+                }
+            }
 
 
 class CommitList(restful.Resource):
